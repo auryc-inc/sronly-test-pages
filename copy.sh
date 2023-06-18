3af549b01b41d3478512b461db5de97bb437587c
Santhosh Sasi <santhosh.sasi@auryc.com>
2023-06-18 15:08:46 -0700

    More fixes and refactoring 2023-06-18

--- copy.sh
+++ copy.sh
@@ -5,7 +5,7 @@ HTTP="/"
 OUTPUT="index.html" 
 
 i=0
-echo "<div style='position:absolute;right: 0;padding: 1em 2em;font-size: 20px; border: 1px solid gray'>"  >$OUTPUT
+echo "<div style='position:absolute;padding: 1em; right: 0;margin: 1em 2em;font-size: 20px; border: 1px solid green; color: green'>"  >$OUTPUT
 echo "<OL>" >> $OUTPUT
 echo "    <li>srOnlyDisableTextCaptureEnabled</li>" >> $OUTPUT
 echo "    <li>disableTextCapture</li>" >> $OUTPUT
