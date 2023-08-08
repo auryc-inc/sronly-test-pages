bb35afc6ced4ab060fd38a19f9971c427133bc5b
Santhosh Sasi <santhosh.sasi@auryc.com>
2023-08-08 16:50:18 -0700

    More fixes and refactoring 2023-08-08

---
+++ qa/auto/test-common.js
@@ -0,0 +1,109 @@
+const urlParams = new URLSearchParams(window.location.search);
+const testId = urlParams.get('q') || 0;
+
+const truthTable = [
+  [1, 0, 0, 1, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [0, 1, 0, 1, 0, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [1, 1, 0, 1, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [1, 0, 1, 1, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [1, 1, 1, 1, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [0, 1, 0, 1, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [1, 1, 1, 0, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [1, 0, 0, 0, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [0, 1, 0, 0, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [1, 0, 1, 0, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [0, 1, 0, 0, 0, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [1, 1, 0, 0, 1, ['1234567890','HIDE_THIS', 'PLAINE_TEXT']],
+  [1, 0, 0, 0, 0, ['1234567890','HIDE_THIS']],
+  [0, 1, 1, 1, 1, ['1234567890','HIDE_THIS']],
+  [0, 1, 1, 1, 0, ['1234567890','HIDE_THIS']],
+  [1, 0, 1, 1, 0, ['1234567890','HIDE_THIS']],
+  [0, 0, 1, 0, 0, ['1234567890','HIDE_THIS']],
+  [0, 0, 1, 0, 1, ['1234567890','HIDE_THIS']],
+  [0, 1, 1, 0, 0, ['1234567890','HIDE_THIS']],
+  [1, 0, 1, 0, 0, ['1234567890','HIDE_THIS']],
+  [0, 0, 1, 1, 0, ['1234567890','HIDE_THIS']],
+  [1, 1, 0, 0, 0, ['1234567890','HIDE_THIS']],
+  [1, 1, 1, 1, 0, ['1234567890','HIDE_THIS']],
+  [0, 0, 1, 1, 1, ['1234567890','HIDE_THIS']],
+  [0, 0, 0, 0, 0, ['1234567890','HIDE_THIS']],
+  [1, 0, 0, 1, 0, ['1234567890','HIDE_THIS']],
+  [1, 1, 0, 1, 0, ['1234567890','HIDE_THIS']],
+  [0, 0, 0, 1, 0, ['1234567890','HIDE_THIS']],
+  [0, 0, 0, 0, 1, ['1234567890','HIDE_THIS']],
+  [0, 1, 1, 0, 1, ['1234567890','HIDE_THIS']],
+  [0, 0, 0, 1, 1, ['1234567890','HIDE_THIS']],
+  [1, 1, 1, 0, 0, ['1234567890','HIDE_THIS']]
+];
+
+const conf = truthTable[testId];
+
+const setConfig = (config) => {
+  window['__AURYC_JSLIB_PATH__'] = 'https://cdn.auryc.dev/libs/latest/';
+  window._auryc_is_inner_frame_ = true;
+  localStorage.setItem('_ar_:record:srOnlyDisableTextCaptureEnabled', !!conf[0]);
+  window.heap = { config: { disableTextCapture: !!conf[1] } };
+  localStorage.setItem('_ar_:behavior:ignoreHeapTextCapture', !!conf[2]);
+  localStorage.setItem('_ar_:record:isRedactTextEnabled', !!conf[3]);
+  localStorage.setItem('_ar_:record:disableSRTextCapture', !!conf[4]);
+};
+
+setConfig();
+
+
+
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const OldWorker = Worker;
+
+window.Worker = function (url) {
+  const inst = new OldWorker(url);
+  const oldListener = inst.postMessage.bind(inst);
+  inst.postMessage = function (...args) {
+    const { data = {} } = args[0] || {};
+    const stringsNotToBeLogged = conf[5] || [];
+    stringsNotToBeLogged.forEach((str) => {
+      if (data.indexOf(str) > -1) {
+        console.error('PII leak -> ', str, '->', data);
+      }
+    });
+    oldListener(...args);
+  };
+  return inst;
+};
+
+function clearAndReload() {
+  localStorage.clear();
+  location.reload();
+  cookieStore.getAll().then((cookies) => {
+    cookies.forEach(({ name }) => {
+      cookieStore.delete(name);
+    });
+  });
+}
+
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const container = document.getElementById('dynami-content');
+    container.innerHTML = '';
+    container.appendChild(document.getElementById('root').cloneNode(true));
+  }, 2000);
+});
+
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const inputs = [...document.querySelectorAll('input')];
+    const timer = setInterval(() => {
+      const input = inputs.shift();
+      if (!input) {
+        clearInterval(timer);
+        return;
+      }
+      const val = input && input.value;
+      input.focus();
+      input.value = val + '-' + testId;
+    }, 300);
+  }, 5000);
+});
