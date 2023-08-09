7491c7fc58e349774e02f80c68a5332a85bfda91
Santhosh Sasi <santhosh.sasi@auryc.com>
2023-08-08 17:33:53 -0700

    More fixes and refactoring 2023-08-08

--- qa/auto/test-common.js
+++ qa/auto/test-common.js
@@ -77,9 +77,8 @@ async function clearAndReload() {
   const cookies = await cookieStore.getAll();
 
   cookies.forEach(async ({ name }) => {
-    const ret1 = await cookieStore.delete({name, domain: 'cdn.auryc.dev', path: '/'});
     const ret2 = await cookieStore.delete({name, domain: 'auryc.dev', path: '/'});
-    console.log('deleted', name, ret1, ret2);
+    console.log('deleted', name, ret2);
   });
 
   location.reload();
